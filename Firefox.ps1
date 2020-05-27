755ba74b3c16e1ef5d077a84b6c8fa3f1c71c5ef
Dmitry Nefedov <farag2@users.noreply.github.com>
2020-05-27 23:30:54 +0300

    Update Firefox.ps1

--- Firefox.ps1
+++ Firefox.ps1
@@ -1,24 +1,16 @@
-﻿# Close Firefox
+# Close Firefox
 $firefox = Get-Process -Name firefox -ErrorAction SilentlyContinue
-IF ($firefox)
+if ($firefox)
 {
 	$firefox.CloseMainWindow()
 }
 Start-Sleep -Seconds 1
-# Get uBlock Origin install path
-$ublock = Get-Item -Path $env:APPDATA\Mozilla\Firefox\Profiles\*default*\extensions\uBlock0@raymondhill.net.xpi
-$prefsjs = (Get-Item -Path $env:APPDATA\Mozilla\Firefox\Profiles\*default*\prefs.js).FullName
-IF ($ublock)
+
+# Check whether extensions installed
+$uBlockOrigin = Get-Item -Path $env:APPDATA\Mozilla\Firefox\Profiles\*default*\extensions\uBlock0@raymondhill.net.xpi
+$defaultbookmarkfolder = Get-Item -Path $env:APPDATA\Mozilla\Firefox\Profiles\*default*\extensions\default-bookmark-folder@gustiaux.com.xpi
+if (-not ($uBlockOrigin) -or (-not ($defaultbookmarkfolder)))
 {
-	# Toolbar with uBlock Origin installed
-	$userjs = 'user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"urlbar-container\",\"home-button\",\"stop-reload-button\",\"bookmarks-menu-button\",\"downloads-button\",\"fxa-toolbar-menu-button\",\"ublock0_raymondhill_net-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"developer-button\",\"ublock0_raymondhill_net-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"widget-overflow-fixed-list\",\"toolbar-menubar\",\"TabsToolbar\",\"PersonalToolbar\"],\"currentVersion\":16,\"newElementCount\":3}");'
-	Add-Content -Path $prefsjs -Value "$userjs" -Force
-}
-Else
-{
-	# Toolbar without uBlock Origin installed
-	$userjs = 'user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"urlbar-container\",\"home-button\",\"stop-reload-button\",\"bookmarks-menu-button\",\"downloads-button\",\"fxa-toolbar-menu-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"developer-button\",\"ublock0_raymondhill_net-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"widget-overflow-fixed-list\",\"toolbar-menubar\",\"TabsToolbar\",\"PersonalToolbar\"],\"currentVersion\":16,\"newElementCount\":3}");'
-	Add-Content -Path $prefsjs -Value "$userjs" -Force
 	$extensions = @(
 		# uBlock Origin
 		"https://addons.mozilla.org/ru/firefox/addon/ublock-origin/",
@@ -27,6 +19,7 @@ Else
 	)
 	Start-Process -FilePath "$env:ProgramFiles\Mozilla Firefox\firefox.exe" -ArgumentList $extensions
 }
+
 # Turn off all scheduled tasks in Mozilla folder
 # Отключить все запланированные задачи в папке Mozilla
 Get-ScheduledTask -TaskPath "\Mozilla\" | Disable-ScheduledTask
