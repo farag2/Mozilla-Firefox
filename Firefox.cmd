ea8a21d010ae82f18f491f1fac0b92e658a906c3
Dmitry Nefedov <10544660+farag2@users.noreply.github.com>
2020-07-02 14:21:23 +0300

    Delete Firefox.cmd

--- Firefox.cmd
+++
@@ -1,5 +0,0 @@
-for /d %%i in (%APPDATA%\Mozilla\Firefox\Profiles\*.default*) do (
-	robocopy "%~dp0." %%i user.js
-	robocopy "%~dp0." %%i search.json.mozlz4
-)
-explorer %APPDATA%\Mozilla\Firefox\Profiles
