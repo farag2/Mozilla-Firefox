70a7cf6f7754723f77318b760e65674012163783
Dmitry Nefedov <nefedovds@ya.ru>
2021-08-19 08:56:04 +0300

    Update user.js

--- user.js
+++ user.js
@@ -42,9 +42,9 @@ user_pref("browser.discovery.enabled", false);
 // Не скрывать кнопку "Загрузки" на панели инструментов
 user_pref("browser.download.autohideButton", false);
 
-// Automatically open downloads when complete
-// Автоматически открывать скачанный файл по завершению скачивания
-user_pref("browser.download.improvements_to_download_panel", true);
+// Do not automatically open downloads when complete
+// Не открывать автоматически скачанный файл по завершению скачивания
+user_pref("browser.download.improvements_to_download_panel", false);
 
 // Turn off counting URIs in private browsing mode
 // Отключить подсчета URI в приватном режиме просмотра
