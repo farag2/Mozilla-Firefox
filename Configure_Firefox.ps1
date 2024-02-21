fcf73119369cb1281832f0f5843ede43b0cf94c6
Dmitry Nefedov <nefedovds@ya.ru>
2024-02-21 22:55:32 +0300

    Update Configure_Firefox.ps1

--- Configure_Firefox.ps1
+++ Configure_Firefox.ps1
@@ -1,3 +1,12 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+if ($Host.Version.Major -eq 5)
+{
+	# Progress bar can significantly impact cmdlet performance
+	# https://github.com/PowerShell/PowerShell/issues/2138
+	$Script:ProgressPreference = "SilentlyContinue"
+}
+
 Get-Process -Name firefox -ErrorAction Ignore | Stop-Process -Force
 Start-Sleep -Seconds 1
 
