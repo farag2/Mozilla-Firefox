15d0e4fb87b4b43e67d79571aef4b8bfec330ebd
Dmitry Nefedov <nefedovds@ya.ru>
2024-03-02 23:45:04 +0300

    Update user.js

--- user.js
+++ user.js
@@ -307,6 +307,10 @@ user_pref("network.trr.bootstrapAddress", "1.1.1.1");
 user_pref("pdfjs.annotationEditorMode", 0);
 user_pref("pdfjs.annotationMode", 2);
 
+// Использовать приложение Windows по умолчанию для открытия PDF-файлов
+// Use Windows default application for PDF files
+user_pref("pdfjs.enabledCache.state", true);
+
 // Enable image PDF editing feature
 // Включить возможность добавлять изображения в режиме редактирования PDF-файлов
 user_pref("pdfjs.enableStampEditor", 2);
