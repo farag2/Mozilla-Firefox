6fc8584b6f59af4ddfde44fedaf32cd29322726e
Dmitry Nefedov <nefedovds@ya.ru>
2024-09-19 23:37:33 +0300

    Update Add_Firefox_Extensions.ps1

--- Add_Firefox_Extensions.ps1
+++ Add_Firefox_Extensions.ps1
@@ -20,14 +20,11 @@ if (-not (Test-Path -Path "$DownloadsFolder\Extensions"))
 
 <#
 	.SYNOPSIS
-	Add extensions to Firefox automatically
-
-	.PARAMETER ExtensionUri
-	Copy URL on an extesion page by right-clicking on the Download button
+	Install extensions to Firefox automatically
 
 	.EXAMPLE
 	$Parameters = @{
-		ExtensionUris = @("https://addons.mozilla.org/firefox/addon/ublock-origin")
+		"https://addons.mozilla.org/firefox/addon/ublock-origin" = "$env:APPDATA\Mozilla\Firefox\Profiles\ProfileName\extensions\uBlock0@raymondhill.net.xpi"
 	}
 	Add-FirefoxExtension @Parameters
 
@@ -36,142 +33,145 @@ if (-not (Test-Path -Path "$DownloadsFolder\Extensions"))
 	Example: https://github.com/farag2/Mozilla-Firefox/discussions/1#discussioncomment-1218530
 
 	.NOTES
+	Leave "ProfileName" in extention installation path as it is
+
+	.NOTES
 	Enable extension manually
 #>
 function Add-FirefoxExtension
 {
-	[CmdletBinding()]
-	param
-	(
-		[Parameter(Mandatory = $true)]
-		[string[]]
-		$ExtensionUris
-	)
-
-	foreach ($Uri in $ExtensionUris)
+	if (-not (Test-Path -Path "$env:APPDATA\Mozilla\Firefox\installs.ini"))
 	{
-		# Downloading extension
-		$Parameters = @{
-			Uri             = $Uri
-			UseBasicParsing = $false # Disabled
-			Verbose         = $true
-		}
-		$Request = Invoke-Webrequest @Parameters
-		$URL = $Request.ParsedHtml.getElementsByClassName("InstallButtonWrapper-download-link") | ForEach-Object -Process {$_.href}
+		Write-Warning -Message "Please launch Firefox first"
+		exit
+	}
 
-		$Extension = Split-Path -Path $URL -Leaf
+	# Get Firefox profile name
+	$String = (Get-Content -Path "$env:APPDATA\Mozilla\Firefox\installs.ini" -Encoding Default | Select-String -Pattern "^\s*Default\s*=\s*.+" | ConvertFrom-StringData).Default
+	$ProfileName = Split-Path -Path $String -Leaf
 
-		$Parameters = @{
-			Uri              = $URL
-			OutFile          = "$DownloadsFolder\Extensions\$Extension"
-			UseBasicParsing  = $true
-			Verbose          = $true
-		}
-		Invoke-WebRequest @Parameters
-
-		# Copy file and rename it into .zip
-		Get-Item -Path "$DownloadsFolder\Extensions\$Extension" -Force | Foreach-Object -Process {
-			Copy-Item -Path $_.FullName -Destination "$DownloadsFolder\Extensions\$($_.BaseName).zip" -Force
-		}
-
-		<#
-			.SYNOPSIS
-			Expand the specific file from ZIP archive. Folder structure will be created recursively
-
-			.Parameter Source
-			The source ZIP archive
-
-			.Parameter Destination
-			Where to expand file
-
-			.Parameter File
-			Assign the file to expand
-
-			.Example
-			ExtractZIPFile -Source "D:\Folder\File.zip" -Destination "D:\Folder" -File "Folder1/Folder2/File.txt"
-		#>
-		function ExtractZIPFile
+	# Parse $Parameters hashtable
+	$Parameters.GetEnumerator() | ForEach-Object -Process {
+		# Replace "ProfileName" with real profile name
+		if (-not (Test-Path -Path $_.Value.Replace("ProfileName", "$ProfileName")))
 		{
-			[CmdletBinding()]
-			param
-			(
-				[string]
-				$Source,
+			# Downloading extension
+			$Parameters = @{
+				Uri             = $_.Name
+				UseBasicParsing = $false # Disabled
+				Verbose         = $true
+			}
+			$Request = Invoke-Webrequest @Parameters
+			$URL = $Request.ParsedHtml.getElementsByClassName("InstallButtonWrapper-download-link") | ForEach-Object -Process {$_.href}
 
-				[string]
-				$Destination,
+			$Extension = Split-Path -Path $URL -Leaf
 
-				[string]
-				$File
-			)
+			$Parameters = @{
+				Uri              = $URL
+				OutFile          = "$DownloadsFolder\Extensions\$Extension"
+				UseBasicParsing  = $true
+				Verbose          = $true
+			}
+			Invoke-WebRequest @Parameters
 
-			Add-Type -Assembly System.IO.Compression.FileSystem
-
-			$ZIP = [IO.Compression.ZipFile]::OpenRead($Source)
-			$Entries = $ZIP.Entries | Where-Object -FilterScript {$_.FullName -eq $File}
-
-			$Destination = "$Destination\$(Split-Path -Path $File -Parent)"
-
-			if (-not (Test-Path -Path $Destination))
-			{
-				New-Item -Path $Destination -ItemType Directory -Force
+			# Copy file and rename it into .zip
+			Get-Item -Path "$DownloadsFolder\Extensions\$Extension" -Force | Foreach-Object -Process {
+				Copy-Item -Path $_.FullName -Destination "$DownloadsFolder\Extensions\$($_.BaseName).zip" -Force
 			}
 
-			$Entries | ForEach-Object -Process {[IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$($Destination)\$($_.Name)", $true)}
+			<#
+				.SYNOPSIS
+				Expand the specific file from ZIP archive. Folder structure will be created recursively
 
-			$ZIP.Dispose()
-		}
+				.Parameter Source
+				The source ZIP archive
 
-		$Parameters = @{
-			Source      = "$DownloadsFolder\Extensions\$Extension"
-			Destination = "$DownloadsFolder\Extensions"
-			File        = "manifest.json"
-		}
-		ExtractZIPFile @Parameters
+				.Parameter Destination
+				Where to expand file
 
-		# Get the author id
-		$manifest = Get-Content -Path "$DownloadsFolder\Extensions\manifest.json" -Encoding Default -Force | ConvertFrom-Json
-		# Some extensions don't have valid JSON manifest
-		if ($manifest.applications.gecko.id)
-		{
-			$ApplicationID = $manifest.applications.gecko.id
-		}
-		elseif ($manifest.browser_specific_settings.gecko.id)
-		{
-			$ApplicationID = $manifest.browser_specific_settings.gecko.id
-		}
+				.Parameter File
+				Assign the file to expand
 
-		if (-not (Test-Path -Path "$DownloadsFolder\Extensions\$ApplicationID.xpi"))
-		{
-			Rename-Item -Path "$DownloadsFolder\Extensions\$Extension" -NewName "$ApplicationID.xpi" -Force
-		}
+				.Example
+				ExtractZIPFile -Source "D:\Folder\File.zip" -Destination "D:\Folder" -File "Folder1/Folder2/File.txt"
+			#>
+			function ExtractZIPFile
+			{
+				[CmdletBinding()]
+				param
+				(
+					[string]
+					$Source,
 
-		# Getting Firefox profile name
-		$String = (Get-Content -Path "$env:APPDATA\Mozilla\Firefox\installs.ini" -Encoding Default | Select-String -Pattern "^\s*Default\s*=\s*.+" | ConvertFrom-StringData).Default
-		$ProfileName = Split-Path -Path $String -Leaf
+					[string]
+					$Destination,
 
-		if (-not (Test-Path -Path "$env:APPDATA\Mozilla\Firefox\Profiles\$ProfileName\extensions"))
-		{
-			New-Item -Path "$env:APPDATA\Mozilla\Firefox\Profiles\$ProfileName\extensions" -ItemType Directory -Force
-		}
+					[string]
+					$File
+				)
 
-		# Copy .xpi extension file to the extensions folder
-		if (-not (Test-Path -Path "$env:APPDATA\Mozilla\Firefox\Profiles\$ProfileName\extensions\$ApplicationID.xpi"))
-		{
-			Copy-Item -Path "$DownloadsFolder\Extensions\$ApplicationID.xpi" -Destination "$env:APPDATA\Mozilla\Firefox\Profiles\$ProfileName\extensions" -Force
+				Add-Type -Assembly System.IO.Compression.FileSystem
+
+				$ZIP = [IO.Compression.ZipFile]::OpenRead($Source)
+				$Entries = $ZIP.Entries | Where-Object -FilterScript {$_.FullName -eq $File}
+
+				$Destination = "$Destination\$(Split-Path -Path $File -Parent)"
+
+				if (-not (Test-Path -Path $Destination))
+				{
+					New-Item -Path $Destination -ItemType Directory -Force
+				}
+
+				$Entries | ForEach-Object -Process {[IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$($Destination)\$($_.Name)", $true)}
+
+				$ZIP.Dispose()
+			}
+
+			$Parameters = @{
+				Source      = "$DownloadsFolder\Extensions\$Extension"
+				Destination = "$DownloadsFolder\Extensions"
+				File        = "manifest.json"
+			}
+			ExtractZIPFile @Parameters
+
+			# Get the author id
+			$manifest = Get-Content -Path "$DownloadsFolder\Extensions\manifest.json" -Encoding Default -Force | ConvertFrom-Json
+			# Some extensions don't have valid JSON manifest
+			if ($manifest.applications.gecko.id)
+			{
+				$ApplicationID = $manifest.applications.gecko.id
+			}
+			elseif ($manifest.browser_specific_settings.gecko.id)
+			{
+				$ApplicationID = $manifest.browser_specific_settings.gecko.id
+			}
+
+			if (-not (Test-Path -Path "$DownloadsFolder\Extensions\$ApplicationID.xpi"))
+			{
+				Rename-Item -Path "$DownloadsFolder\Extensions\$Extension" -NewName "$ApplicationID.xpi" -Force
+			}
+
+			if (-not (Test-Path -Path "$env:APPDATA\Mozilla\Firefox\Profiles\$ProfileName\extensions"))
+			{
+				New-Item -Path "$env:APPDATA\Mozilla\Firefox\Profiles\$ProfileName\extensions" -ItemType Directory -Force
+			}
+
+			# Copy .xpi extension file to the extensions folder
+			if (-not (Test-Path -Path "$env:APPDATA\Mozilla\Firefox\Profiles\$ProfileName\extensions\$ApplicationID.xpi"))
+			{
+				Copy-Item -Path "$DownloadsFolder\Extensions\$ApplicationID.xpi" -Destination "$env:APPDATA\Mozilla\Firefox\Profiles\$ProfileName\extensions" -Force
+			}
 		}
 	}
 }
 
+# Retrive extensions' ID to check path for
 $Parameters = @{
-	ExtensionUris = @(
-		"https://addons.mozilla.org/firefox/addon/ublock-origin",
-		"https://addons.mozilla.org/firefox/addon/traduzir-paginas-web",
-		# "https://addons.mozilla.org/firefox/addon/tampermonkey",
-		# https://greasyfork.org/ru/scripts/436115-return-youtube-dislike
-		"https://addons.mozilla.org/firefox/addon/sponsorblock",
-		"https://addons.mozilla.org/firefox/addon/return-youtube-dislikes"
-	)
+	"https://addons.mozilla.org/firefox/addon/ublock-origin"           = "$env:APPDATA\Mozilla\Firefox\Profiles\ProfileName\extensions\uBlock0@raymondhill.net.xpi"
+	"https://addons.mozilla.org/firefox/addon/traduzir-paginas-web"    = "$env:APPDATA\Mozilla\Firefox\Profiles\ProfileName\extensions\{036a55b4-5e72-4d05-a06c-cba2dfcc134a}.xpi"
+	"https://addons.mozilla.org/firefox/addon/sponsorblock"            = "$env:APPDATA\Mozilla\Firefox\Profiles\ProfileName\extensions\sponsorBlocker@ajay.app.xpi"
+	# https://greasyfork.org/ru/scripts/436115-return-youtube-dislike
+	"https://addons.mozilla.org/firefox/addon/return-youtube-dislikes" = "$env:APPDATA\Mozilla\Firefox\Profiles\ProfileName\extensions\{762f9885-5a13-4abd-9c77-433dcd38b8fd}.xpi"
 }
 Add-FirefoxExtension @Parameters
 
